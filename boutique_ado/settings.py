1a42c426be027a7faba7d157ac9b71619924b04a
flashdrag <flashdrag@gmail.com>
2023-07-23 16:33:49 +0100

    Add `home` app

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'users',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -71,7 +72,10 @@ ROOT_URLCONF = 'boutique_ado.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
