6eaf37c7b0510bcdc193b5d5bc8d5908c0b11a8f
flashdrag <flashdrag@gmail.com>
2023-07-25 15:32:49 +0100

    Add product detail page

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Product
 
 
@@ -11,3 +11,13 @@ def all_products(request):
         'products': products,
     }
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    '''A view to show individual product detail'''
+
+    product = get_object_or_404(Product, pk=product_id)
+    context = {
+        'product': product,
+    }
+    return render(request, 'products/product_detail.html', context)
