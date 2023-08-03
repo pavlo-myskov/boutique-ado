1132ea137967d9249ac6aa0e69c4703b16d2765b
flashdrag <flashdrag@gmail.com>
2023-08-03 16:45:54 +0100

    Display sizes in the bag

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,28 +1,40 @@
 from decimal import Decimal
 from django.conf import settings
 from django.shortcuts import get_object_or_404
-
 from products.models import Product
 
 
 def bag_contents(request):
-    """
-    This function makes the bag contents available to all templates
-    """
     bag_items = []
     total = 0
     product_count = 0
-    bag = request.session.get('bag', {})
+    bag = request.session.get("bag", {})
 
-    for item_id, quantity in bag.items():
-        product = get_object_or_404(Product, pk=item_id)
-        total += quantity * product.price
-        product_count += quantity
-        bag_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'product': product,
-        })
+    for item_id, item_data in bag.items():
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.price
+            product_count += item_data
+            bag_items.append(
+                {
+                    "item_id": item_id,
+                    "quantity": item_data,
+                    "product": product,
+                }
+            )
+        else:
+            product = get_object_or_404(Product, pk=item_id)
+            for size, quantity in item_data["items_by_size"].items():
+                total += quantity * product.price
+                product_count += quantity
+                bag_items.append(
+                    {
+                        "item_id": item_id,
+                        "quantity": item_data,
+                        "product": product,
+                        "size": size,
+                    }
+                )
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
@@ -34,13 +46,13 @@ def bag_contents(request):
     grand_total = delivery + total
 
     context = {
-        'bag_items': bag_items,
-        'total': total,
-        'product_count': product_count,
-        'delivery': delivery,
-        'free_delivery_delta': free_delivery_delta,
-        'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
-        'grand_total': grand_total,
+        "bag_items": bag_items,
+        "total": total,
+        "product_count": product_count,
+        "delivery": delivery,
+        "free_delivery_delta": free_delivery_delta,
+        "free_delivery_threshold": settings.FREE_DELIVERY_THRESHOLD,
+        "grand_total": grand_total,
     }
 
     return context
--- bag/views.py
+++ bag/views.py
@@ -10,11 +10,23 @@ def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag"""
 
     quantity = int(request.POST.get('quantity'))
+    size = request.POST.get('product_size')
     redirect_url = request.POST.get('redirect_url')
 
-    # Store the quantity of the item to the session
     bag = request.session.get('bag', {})
-    bag[item_id] = bag.get(item_id, 0) + quantity
+
+    if size:
+        if item_id in list(bag.keys()):
+            if size in bag[item_id]['items_by_size'].keys():
+                bag[item_id]['items_by_size'][size] += quantity
+            else:
+                bag[item_id]['items_by_size'][size] = quantity
+        else:
+            bag[item_id] = {'items_by_size': {size: quantity}}
+    else:
+        # Store the quantity of the item to the session
+        bag[item_id] = bag.get(item_id, 0) + quantity
+
     request.session['bag'] = bag
 
     return redirect(redirect_url)
