ca139a2e4dea12621dd09aef07886f1190ee20ec
flashdrag <flashdrag@gmail.com>
2023-08-04 19:22:23 +0100

    Implement removing product functionality

--- bag/urls.py
+++ bag/urls.py
@@ -3,7 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.view_bag, name='view_bag'),
-    path('add/<str:item_id>', views.add_to_bag, name='add_to_bag'),
-    path('adjust/<str:item_id>', views.adjust_bag, name='adjust_bag'),
+    path("", views.view_bag, name="view_bag"),
+    path("add/<str:item_id>", views.add_to_bag, name="add_to_bag"),
+    path("adjust/<str:item_id>", views.adjust_bag, name="adjust_bag"),
+    path(
+        "remove/<str:item_id>", views.remove_from_bag, name="remove_from_bag"
+    ),
 ]
