c67cbcecb0bd4897bf7b30dbcf987bfa97f0437e
flashdrag <flashdrag@gmail.com>
2023-08-25 20:00:59 +0100

    Refactor image field: create custom ClearableFileInput

--- products/forms.py
+++ products/forms.py
@@ -1,12 +1,17 @@
 from django import forms
 
+from .widgets import CustomClearableFileInput
 from .models import Product, Category
 
 
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        fields = '__all__'
+        fields = "__all__"
+
+    image = forms.ImageField(
+        label="Image", required=False, widget=CustomClearableFileInput
+    )
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -15,7 +20,7 @@ class ProductForm(forms.ModelForm):
         friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
 
         # Update the category field on the form to use friendly names
-        self.fields['category'].choices = friendly_names
+        self.fields["category"].choices = friendly_names
         # add class to fields to match the theme of the website
         for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'border-black rounded-0'
+            field.widget.attrs["class"] = "border-black rounded-0"
