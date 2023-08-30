cbdaee526de027c6ea4bf50435ace1ff6944fae1
flashdrag <flashdrag@gmail.com>
2023-08-30 19:31:55 +0100

    Fix the path to custom_storages

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -183,22 +183,21 @@ if not DEVELOPMENT:
     AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
-    AWS_S3_REGION_NAME = 'eu-west-1'
-    AWS_DEFAULT_ACL = None
+    AWS_S3_REGION_NAME = 'eu-west-2'
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 
     # s3 static settings
-    AWS_LOCATION = 'static'
+    STATICFILES_LOCATION = 'static'
     # URL path for your static files where they will be served from
-    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
-    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    STATICFILES_STORAGE = 'boutique_ado.custom_storages.StaticStorage'
 
     # s3 media settings
-    MEDIA_LOCATION = 'media'
+    MEDIAFILES_LOCATION = 'media'
     # URL path for media files where they will be served from
-    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIA_LOCATION}/'
-    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
+    DEFAULT_FILE_STORAGE = 'boutique_ado.custom_storages.MediaStorage'
 else:
     # URL path for your static files where they
     # will be served from during development
