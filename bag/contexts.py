47bd3101fb3a3c4e12155340a0339dc726c85762
flashdrag <flashdrag@gmail.com>
2023-09-24 18:35:37 +0100

    Fix bag: remove non-existing item from the bag

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,6 +1,5 @@
 from decimal import Decimal
 from django.conf import settings
-from django.shortcuts import get_object_or_404
 from products.models import Product
 
 
@@ -12,29 +11,41 @@ def bag_contents(request):
 
     for item_id, item_data in bag.items():
         if isinstance(item_data, int):
-            product = get_object_or_404(Product, pk=item_id)
-            total += item_data * product.price
-            product_count += item_data
-            bag_items.append(
-                {
-                    "item_id": item_id,
-                    "quantity": item_data,
-                    "product": product,
-                }
-            )
-        else:
-            product = get_object_or_404(Product, pk=item_id)
-            for size, quantity in item_data["items_by_size"].items():
-                total += quantity * product.price
-                product_count += quantity
+            try:
+                product = Product.objects.get(pk=item_id)
+                total += item_data * product.price
+                product_count += item_data
                 bag_items.append(
                     {
                         "item_id": item_id,
-                        "quantity": quantity,
+                        "quantity": item_data,
                         "product": product,
-                        "size": size,
                     }
                 )
+            except Product.DoesNotExist:
+                # remove non-existing item from the bag
+                bag.pop(item_id)
+                request.session["bag"] = bag
+                request.session.modified = True
+        else:
+            try:
+                product = Product.objects.get(pk=item_id)
+                for size, quantity in item_data["items_by_size"].items():
+                    total += quantity * product.price
+                    product_count += quantity
+                    bag_items.append(
+                        {
+                            "item_id": item_id,
+                            "quantity": quantity,
+                            "product": product,
+                            "size": size,
+                        }
+                    )
+            except Product.DoesNotExist:
+                # remove non-existing item from the bag
+                bag.pop(item_id)
+                request.session["bag"] = bag
+                request.session.modified = True
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
